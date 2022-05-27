e081a79a5ca2a9ec1ee787f4965992cb187e7956
Donovan Shook <donovan.shook@sstid.com>
2022-05-27 17:26:01 -0400

    Capture errors that occur during before hook

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-'use strict'
-
-const WdioReporter = require('@wdio/reporter').default
-const assert = require('assert')
-const fs = require('fs')
-const path = require('path')
-const Readable = require('stream').Readable
-const { v4: uuid } = require('uuid')
-const shell = require('shelljs')
+const WdioReporter = require('@wdio/reporter').default;
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const Readable = require('stream').Readable;
+const { v4: uuid } = require('uuid');
+const shell = require('shelljs');
 
 /**
  * @typedef {Object} SuiteStats
@@ -38,9 +36,21 @@ const shell = require('shelljs')
  * @property {string} [end]
  */
 
+/**
+ * @typedef {Object} HookStats
+ * @property {string} type
+ * @property {string} start
+ * @property {number} _duration
+ * @property {string} uid
+ * @property {string} cid
+ * @property {string} title
+ * @property {string} parent
+ * @property {string} [end]
+ */
+
 class WdioTeamcityReporter extends WdioReporter {
   static escape (str) {
-    if (!str) return ''
+    if (!str) return '';
     return str
       .toString()
       .replace(/\|/g, '||')
@@ -51,59 +61,68 @@ class WdioTeamcityReporter extends WdioReporter {
       .replace(/\u0085/g, '|x') // next line
       .replace(/\u2028/g, '|l') // line separator
       .replace(/\u2029/g, '|p') // paragraph separator
-      .replace(/'/g, '|\'')
+      .replace(/'/g, '|\'');
   }
 
   static bool (value, fallback) {
-    return typeof value === 'boolean' ? value : fallback
+    return typeof value === 'boolean' ? value : fallback;
   }
 
   static number (value, fallback) {
-    return typeof value === 'number' ? value : fallback
+    return typeof value === 'number' ? value : fallback;
   }
 
   static string (value, fallback) {
-    return typeof value === 'string' ? value : fallback
+    return typeof value === 'string' ? value : fallback;
   }
 
   constructor (reporterOptions) {
-    const r = WdioTeamcityReporter
+    const r = WdioTeamcityReporter;
     const params = {
       captureStandardOutput: r.bool(reporterOptions.captureStandardOutput, false),
       flowId: r.bool(reporterOptions.flowId, true),
       message: r.string(reporterOptions.message, '[title]'),
       screenshotPath: r.string(reporterOptions.screenshotPath, 'temp/screenshots/'),
       stdout: true,
-      writeStream: process.stdout
-    }
-    const options = Object.assign(reporterOptions, params)
+      writeStream: process.stdout,
+    };
+    const options = Object.assign(reporterOptions, params);
 
-    super(options)
+    super(options);
 
-    const folderPath = path.join(process.cwd(), params.screenshotPath)
+    const folderPath = path.join(process.cwd(), params.screenshotPath);
 
-    shell.mkdir('-p', folderPath)
+    shell.mkdir('-p', folderPath);
 
-    this.fullScreenshotPath = folderPath
-    this.currentTestStats = null
-    this.previousTestUid = null
-    this.iterator = 0
+    this.fullScreenshotPath = folderPath;
+    this.currentTestStats = null;
+    this.previousTestUid = null;
+    this.iterator = 0;
   }
 
   /**
    * @param {SuiteStats} suiteStats
    */
   onSuiteStart (suiteStats) {
-    this._m('##teamcity[testSuiteStarted name=\'{name}\' flowId=\'{id}\']', suiteStats)
+    this._m('##teamcity[testSuiteStarted name=\'{name}\' flowId=\'{id}\']', suiteStats);
   }
 
   /**
    * @param {TestStats} testStats
    */
   onTestStart (testStats) {
-    this.currentTestStats = testStats
+    this.currentTestStats = testStats;
 
-    this._m('##teamcity[testStarted name=\'{name}\' captureStandardOutput=\'{capture}\' flowId=\'{id}\']', testStats)
+    this._m('##teamcity[testStarted name=\'{name}\' captureStandardOutput=\'{capture}\' flowId=\'{id}\']', testStats);
+  }
+
+  /**
+   * @param {HookStats} hookStats
+   */
+  onHookEnd (hookStats) {
+    if (hookStats.currentTest !== undefined && hookStats.state === 'failed') {
+      this._m('##teamcity[testFailed name=\'{name}\' message=\'{error}\' details=\'{stack}\' flowId=\'{id}\']', hookStats);
+    }
   }
 
   /**
@@ -111,24 +130,24 @@ class WdioTeamcityReporter extends WdioReporter {
    */
   onTestEnd (testStats) {
     if (testStats.state === 'skipped') {
-      return
+      return;
     }
-    this._m('##teamcity[testFinished name=\'{name}\' duration=\'{ms}\' flowId=\'{id}\']', testStats)
+    this._m('##teamcity[testFinished name=\'{name}\' duration=\'{ms}\' flowId=\'{id}\']', testStats);
   }
 
   /**
    * @param {TestStats} testStats
    */
   onTestFail (testStats) {
-    const { escape, number } = WdioTeamcityReporter
-    const specFileRetryAttempts = number(this.runnerStat.config.specFileRetryAttempts, 0)
-    const specFileRetries = number(this.runnerStat.config.specFileRetries, 0)
-    const attempt = escape(`${specFileRetryAttempts}/${specFileRetries}`)
+    const { escape, number } = WdioTeamcityReporter;
+    const specFileRetryAttempts = number(this.runnerStat.config.specFileRetryAttempts, 0);
+    const specFileRetries = number(this.runnerStat.config.specFileRetries, 0);
+    const attempt = escape(`${specFileRetryAttempts}/${specFileRetries}`);
 
     if (specFileRetryAttempts === specFileRetries) {
-      this._m('##teamcity[testFailed name=\'{name}\' message=\'{error}\' details=\'{stack}\' flowId=\'{id}\']', testStats)
+      this._m('##teamcity[testFailed name=\'{name}\' message=\'{error}\' details=\'{stack}\' flowId=\'{id}\']', testStats);
     } else {
-      this._m(`##teamcity[message name='{name}' text='attempt ${attempt} failed: {error}' flowId='{id}']`, testStats)
+      this._m(`##teamcity[message name='{name}' text='attempt ${attempt} failed: {error}' flowId='{id}']`, testStats);
     }
   }
 
@@ -136,42 +155,42 @@ class WdioTeamcityReporter extends WdioReporter {
    * @param {TestStats} testStats
    */
   onTestSkip (testStats) {
-    this._m('##teamcity[testIgnored name=\'{name}\' message=\'skipped\' flowId=\'{id}\']', testStats)
+    this._m('##teamcity[testIgnored name=\'{name}\' message=\'skipped\' flowId=\'{id}\']', testStats);
   }
 
   /**
    * @param {SuiteStats} suiteStats
    */
   onSuiteEnd (suiteStats) {
-    this._m('##teamcity[testSuiteFinished name=\'{name}\' flowId=\'{id}\']', suiteStats)
+    this._m('##teamcity[testSuiteFinished name=\'{name}\' flowId=\'{id}\']', suiteStats);
   }
 
   onAfterCommand (command) {
-    const screenshotRegEx = /\/session\/[^/]*\/screenshot/
+    const screenshotRegEx = /\/session\/[^/]*\/screenshot/;
 
     // If this is a screenshot command and there is an associated value (the screenshot data)
     if (screenshotRegEx.test(command.endpoint) && command.result.value) {
       // Save screenshot to path (TODO: allow path to be defined in initial config)
       if (this.currentTestStats.uid === this.previousTestUid) {
-        this.iterator++
+        this.iterator++;
       } else {
-        this.iterator = 0
+        this.iterator = 0;
       }
 
-      const uuidFilename = uuid()
-      const bufferData = Buffer.from(command.result.value, 'base64')
-      const streamData = new Readable()
-      const fileName = `${this.iterator}-${uuidFilename}.png`
-      const filePath = path.join(this.fullScreenshotPath, fileName)
-      streamData.push(bufferData)
-      streamData.push(null)
-      streamData.pipe(fs.createWriteStream(filePath))
+      const uuidFilename = uuid();
+      const bufferData = Buffer.from(command.result.value, 'base64');
+      const streamData = new Readable();
+      const fileName = `${this.iterator}-${uuidFilename}.png`;
+      const filePath = path.join(this.fullScreenshotPath, fileName);
+      streamData.push(bufferData);
+      streamData.push(null);
+      streamData.pipe(fs.createWriteStream(filePath));
 
-      const screenshotDisplayName = `Screenshot ${this.iterator + 1}`
+      const screenshotDisplayName = `Screenshot ${this.iterator + 1}`;
 
-      this._m(`##teamcity[testMetadata name='${screenshotDisplayName}' type='image' value='${fileName}' flowId='{id}']`, this.currentTestStats)
+      this._m(`##teamcity[testMetadata name='${screenshotDisplayName}' type='image' value='${fileName}' flowId='{id}']`, this.currentTestStats);
 
-      this.previousTestUid = this.currentTestStats.uid
+      this.previousTestUid = this.currentTestStats.uid;
     }
   }
 
@@ -180,40 +199,40 @@ class WdioTeamcityReporter extends WdioReporter {
    * @param {TestStats | SuiteStats} stats
    */
   _m (template, stats) {
-    assert(stats != null, '_m(): missing stats argument')
+    assert(stats != null, '_m(): missing stats argument');
 
     if (!this.options.flowId) {
-      template = template.replace(' flowId=\'{id}\'', '')
+      template = template.replace(' flowId=\'{id}\'', '');
     }
 
     const fragment = pattern => {
       switch (pattern) {
-        case '{capture}':
-          return this.options.captureStandardOutput ? 'true' : 'false'
-        case '{id}':
-          return this.runnerStat.sessionId + '/' + stats.cid
-        case '{ms}':
-          return stats._duration
-        case '{name}':
-          var name = this.options.message
-          if (name.includes('[browser]')) name = name.replace(/\[browser\]/g, this._v())
-          if (name.includes('[title]')) name = name.replace(/\[title\]/g, stats.title)
-          return name
-        case '{state}':
-          return stats.state
-        case '{error}':
-          return stats.error.message
-        case '{stack}':
-          return stats.error.stack
-        default:
-          return ''
+      case '{capture}':
+        return this.options.captureStandardOutput ? 'true' : 'false';
+      case '{id}':
+        return this.runnerStat.sessionId + '/' + stats.cid;
+      case '{ms}':
+        return stats._duration;
+      case '{name}':
+        var name = this.options.message;
+        if (name.includes('[browser]')) name = name.replace(/\[browser\]/g, this._v());
+        if (name.includes('[title]')) name = name.replace(/\[title\]/g, stats.title);
+        return name;
+      case '{state}':
+        return stats.state;
+      case '{error}':
+        return stats.error.message;
+      case '{stack}':
+        return stats.error.stack;
+      default:
+        return '';
       }
-    }
+    };
 
     const m = template.replace(/\{[a-z]+\}/gi, m =>
-      WdioTeamcityReporter.escape(fragment(m)))
+      WdioTeamcityReporter.escape(fragment(m)));
 
-    this.write(m + '\n')
+    this.write(m + '\n');
   }
 
   _v () {
@@ -223,5 +242,5 @@ class WdioTeamcityReporter extends WdioReporter {
   }
 }
 
-module.exports.default = WdioTeamcityReporter
-module.exports.reporterName = 'teamcity'
+module.exports.default = WdioTeamcityReporter;
+module.exports.reporterName = 'teamcity';
